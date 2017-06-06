2f8b88fda676c6451be5a3052ca457fc4bc01222
zainab <zainysham@gmail.com>
2017-06-06 10:56:25 +0100

    created a json file and corrected some test usecases

--- app/library.js
+++ app/library.js
@@ -1,30 +1,31 @@
 'use strict';
 
 module.exports = {
- getPrimes: (n) => {
- /* Find the prime numbers less than the parameter given */
- let PrimeNumbers = [];
- var isprime;
-   
-   if (isNaN(n)=== true || n<1 || n===0 || n===1){
-   	return false;
+	 getPrimes: (n) => {
+	 /* Find the prime numbers less than the parameter given */
+	 let PrimeNumbers = [];
+	 var isprime;
+	   
+	   if (isNaN(n)=== true || n<1 || n===0 || n===1){
+	   	return false;
 
-   } else {
-   // your logic goes here
-	  for (var i=2; i<=parseInt(n); i++){
-	    isprime=true;
-	  	for (var j=2; j<i; j++)
-	  	{
-	  		if (i%j === 0){
-	  		  isprime = false;
-	  			break;
-	  		} 
-				
-	  	}
-	  	if (isprime === true){
-	    PrimeNumbers.push(i);
-	  	}
-	  }
-	   return PrimeNumbers;
+	   } else {
+	   // your logic goes here
+		  for (var i=2; i<=parseInt(n); i++){
+		    isprime=true;
+		  	for (var j=2; j<i; j++)
+		  	{
+		  		if (i%j === 0){
+		  		  isprime = false;
+		  			break;
+		  		} 
+					
+		  	}
+		  	if (isprime === true){
+		    PrimeNumbers.push(i);
+		  	}
+		  }
+		   return PrimeNumbers;
+		}
 	}
-},
+}
