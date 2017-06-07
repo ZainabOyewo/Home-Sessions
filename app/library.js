c77afddcad957d188f8f2074435e9104fdf6f6c6
zainaboyewo <zainysham@gmail.com>
2017-06-07 20:30:06 +0100

    Update Library with comments

--- app/library.js
+++ app/library.js
@@ -3,21 +3,28 @@
 module.exports = {
 	reverseString: (word) => {
 		
-		  var revword;
-
+		  var revword; // to hold the reverse strings
+		
+		//Check if the word parameter is empty
 		  if (word.length===0){
 		    return null;
 		  }
+		  
+		  //check if word is a string
 		  if(isNaN(word) === false){
 		    return "this is a number not a word";
 		  }
-
+		  
+		//declare revword empty string
 		  revword="";
+		  
+		  //loop through each character starting from the last character
 		  for (var i=(word.length - 1); i>=0; i--){
-		    revword = revword + word[i];
+		  
+		    revword = revword + word[i];  //attach each character to revword
 		    }
 
-
+			//return true if the reverese string gotten is same as the original word
 		    if (revword === word){
 		      return true;
 		    } 
