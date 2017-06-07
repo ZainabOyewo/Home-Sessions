866a48192ca043126075fd703b4ab53bd3d608a5
zainaboyewo <zainysham@gmail.com>
2017-06-07 21:08:51 +0100

    Update library by addng comment

--- app/library.js
+++ app/library.js
@@ -1,10 +1,11 @@
 'use strict';
-
+	//This Car class is to pass all the written tests
+	
 	function Car (name,model,type) {
 
 		this.name = name || 'General';
 		this.model = model || 'GM';
-		this.type= type || 'General'
+		this.type= type || 'General';
 		this.isSaloon = (type === 'trailer') ? false : true;
 		this.numOfWheels= this.isSaloon ? 4: 8 ; 
 		this.numOfDoors = (name === 'Porshe') || (name === 'Koenigsegg') ? 2 : 4;
